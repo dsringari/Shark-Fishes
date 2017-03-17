cd10b15f5de82c69de6ef0dae77f3b29969bbc82
Dhruv Sringari <dhruvy427@gmail.com>
2017-03-17 17:28:52 -0400

    Creates the fishes and the playground file

---
+++ Shark&Fishes.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import SpriteKit
+
+var str = "Hello, playground"
+let frame = CGRect(x: 0, y: 0, width: 1024, height: 768)
+var scene = SKScene(size: frame.size)
+
+
